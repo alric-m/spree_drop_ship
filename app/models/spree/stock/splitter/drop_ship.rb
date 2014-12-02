ba53cfcd055591ca2dedf93bc3de8e453ac876c3
alric-m <matthias.alric@epitech.eu>
2014-12-02 17:07:48 +0100

    puts

--- app/models/spree/stock/splitter/drop_ship.rb
+++ app/models/spree/stock/splitter/drop_ship.rb
@@ -27,8 +27,8 @@ module Spree
                 puts content
                 puts "stock_location"
                 puts stock_location
-                puts "supplier"
-                puts supplier
+                puts "supplier id"
+                puts supplier.id
                 puts "___________________________"
                 split_packages << Spree::Stock::Package.new(stock_location, [content])
                 # split_packages << Spree::Stock::Package.new(stock_location, current_order, [content])
