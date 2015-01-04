f346dcdf8c2c1f9886a16b4b731bc018428dbd62
alric-m <matthias.alric@epitech.eu>
2015-01-04 17:18:16 +0100

    d2d email

--- app/mailers/spree/drop_ship_order_mailer.rb
+++ app/mailers/spree/drop_ship_order_mailer.rb
@@ -17,7 +17,7 @@ module Spree
     def door_to_door_order(shipment_id)
       @shipment = Shipment.find shipment_id
       @supplier = @shipment.supplier
-      mail to: @supplier.email, subject: 'Door2Door'
+      mail to: @supplier.email, subject: Spree.t('door_to_door_order_mailer.supplier_order.subject', name: Spree::Store.current.name, number: @shipment.number)
     end
 
   end
